bac5368263dfcfb10831bdcbf77e90b2b9247102
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2013-10-17 09:34:31 +0900

    TypeSketchのテストの構成を変更 refs #9

--- ReflectionExt.Tests/TypeSketchTest.cs
+++ ReflectionExt.Tests/TypeSketchTest.cs
@@ -8,39 +8,54 @@ namespace ReflectionExt.Tests
     [TestFixture]
     public class TypeSketchTest
     {
-        [Test]
-        public void ApplyTypesでintを表すTypeSketchからClosedTypeに変換できる()
+        public class intを表すTypeSketch
         {
-            ClosedType sut = Reflect.Type<int>().ApplyTypes();
-            Assert.That(sut.ToType(), Is.EqualTo(typeof(int)));
+            [Test]
+            public void ApplyTypesでClosedTypeに変換できる()
+            {
+                ClosedType sut = Reflect.Type<int>().ApplyTypes();
+                Assert.That(sut.ToType(), Is.EqualTo(typeof(int)));
+            }
         }
 
-        [Test]
-        public void ApplyTypesでintのSeqを表すTypeSketchからClosedTypeに変換できる()
+        public class intのSeqを表すTypeSketch
         {
-            ClosedType sut = Reflect.Type<Seq<int>>().ApplyTypes();
-            Assert.That(sut.ToType(), Is.EqualTo(typeof(Seq<int>)));
+            [Test]
+            public void ApplyTypesでClosedTypeに変換できる()
+            {
+                ClosedType sut = Reflect.Type<Seq<int>>().ApplyTypes();
+                Assert.That(sut.ToType(), Is.EqualTo(typeof(Seq<int>)));
+            }
         }
 
-        [Test]
-        public void ApplyTypesでSeqを表すTypeSketchからClosedTypeに変換できる()
+        public class Seqを表すTypeSketch
         {
-            ClosedType sut = Reflect.Type(typeof(Seq<>)).ApplyTypes(Reflect.Type<int>().ApplyTypes());
-            Assert.That(sut.ToType(), Is.EqualTo(typeof(Seq<int>)));
+            [Test]
+            public void ApplyTypesでClosedTypeに変換できる()
+            {
+                ClosedType sut = Reflect.Type(typeof(Seq<>)).ApplyTypes(Reflect.Type<int>().ApplyTypes());
+                Assert.That(sut.ToType(), Is.EqualTo(typeof(Seq<int>)));
+            }
         }
 
-        [Test]
-        public void ApplyTypesで型パラメータの数が多すぎる場合は例外が発生する()
+        public class 型パラメータが多すぎる場合
         {
-            var sut = Reflect.Type<Seq<int>>();
-            Assert.That(() => sut.ApplyTypes(Reflect.Type<int>().ApplyTypes()), Throws.Exception.TypeOf<ArgumentException>());
+            [Test]
+            public void ApplyTypesで例外が発生する()
+            {
+                var sut = Reflect.Type<Seq<int>>();
+                Assert.That(() => sut.ApplyTypes(Reflect.Type<int>().ApplyTypes()), Throws.Exception.TypeOf<ArgumentException>());
+            }
         }
 
-        [Test]
-        public void ApplyTypesで型パラメータの数が少なすぎる場合は例外が発生する()
+        public class 型パラメータが少なすぎる場合
         {
-            var sut = Reflect.Type(typeof(Seq<>));
-            Assert.That(() => sut.ApplyTypes(), Throws.Exception.TypeOf<ArgumentException>());
+            [Test]
+            public void ApplyTypesで例外が発生する()
+            {
+                var sut = Reflect.Type(typeof(Seq<>));
+                Assert.That(() => sut.ApplyTypes(), Throws.Exception.TypeOf<ArgumentException>());
+            }
         }
 
         [TestCase(typeof(Seq<int>), typeof(Seq<>))]
